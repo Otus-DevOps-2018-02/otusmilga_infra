ec6e8a48dc592f2e5c7a619013ca126a3d5a2961
Andy Milga <otus@milga.space>
2018-03-30 20:42:06 +0300

    deleted human readable output

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
 
 function run_c {
     cmd_output=$($@)
     local status=$?
     if [ $status -ne 0 ]; then
-        echo "${red}[FAILED]${reset}... $@"
+        echo "[FAILED]... $@"
 	state_fail=1
     fi
-    echo "${green}[OK]${reset}... $@ >&2"
+    echo "[OK]... $@"
 }
 
 run_c git clone -b monolith https://github.com/express42/reddit.git
@@ -20,12 +17,10 @@ run_c bundle install
 run_c puma -d
 
 if [[ $state_fail -eq 1 ]]; then
-    echo ""
-    echo "${red}[FAIL]${reset} Script $0 failed. Check error log below."
+    echo "[FAIL] Script $0 failed. Check error log below."
     exit 1
 else
-    echo ""
-    echo "${green}[OK]${reset}:Monolith reddit_app(https://github.com/express42/reddit.git) installed and started."
+    echo "[OK]:Monolith reddit_app(https://github.com/express42/reddit.git) installed and started."
 fi
 exit 0
 
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
-
 function run_c {
     cmd_output=$($@)
     local status=$?
     if [ $status -ne 0 ]; then
-        echo "${red}[FAILED]${reset}... $@"
+        echo "[FAILED]... $@"
 	state_fail=1
     fi
-    echo "${green}[OK]${reset}... $@" >&2
+    echo "[OK]... $@"
 }
 
 run_c sudo apt-get --yes update 
@@ -19,18 +15,13 @@ run_c sudo apt-get install --yes ruby-full ruby-bundler build-essential
 run_c ruby -v 
 run_c bundle -v
 
-BUNDLER_VER='bundle -v'
-RUBY_VER=$(ruby -v | awk {'print $2'})
-RUBY_DEST=$(which ruby)
-BUNDLER_VER=$(bundle -v | awk {'print $3'})
-
 if [[ $state_fail -eq 1 ]]; then
     echo ""
-    echo "${red}[FAIL]${reset} Script $0 failed. Check error log below."
+    echo "[FAIL] Script $0 failed. Check error log below."
     exit 1
 else
     echo ""
-    echo "${green}[OK]${reset}:Ruby: ${RUBY_VER} ruby at ${RUBY_DEST} installed with bundler (${BUNDLER_VER})"
+    echo "[OK]:Ruby: ruby with bundler installed"
 fi
 exit 0
 
